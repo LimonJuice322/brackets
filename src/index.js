169a19d5ee491db93a4901c1725764a1e159e965
LJ322 <general00760@gmail.com>
2020-10-06 01:45:35 +0300

    feat: complete all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+    const brackets = {};
+    bracketsConfig.forEach(item => brackets[`${item[0]}`] = item[1]);
+
+    for (let i = 0; i < str.length; i++) {
+        let current = str[i];
+        if (stack[stack.length-1] == current) stack.pop();
+        else if (brackets[current]) stack.push(brackets[current]);
+        else return false;
+    }
+    return (stack.length) ? false : true
 }
